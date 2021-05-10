1251bf42d1489f2021e7baa775e97abf83428f8a
Erick Medina <emedina@bemobile.es>
2021-05-10 22:18:33 +0200

    adjust theme and toolbar

---
+++ app/src/main/java/tech/medina/adichallenge/ui/review/detail/ReviewDetailActivity.kt
@@ -0,0 +1,30 @@
+package tech.medina.adichallenge.ui.review.detail
+
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModel
+import tech.medina.adichallenge.ui.product.detail.ProductDetailActivity
+import tech.medina.adichallenge.databinding.ActivityItemListBinding
+import tech.medina.adichallenge.ui.common.BaseActivity
+import tech.medina.adichallenge.ui.product.list.ProductListActivity
+
+/**
+ * An activity representing a single Item detail screen. This
+ * activity is only used on narrow width devices. On tablet-size devices,
+ * item details are presented side-by-side with a list of items
+ * in a [ProductListActivity].
+ */
+class ReviewDetailActivity : BaseActivity() {
+
+    private lateinit var binding: ActivityItemListBinding
+
+    override fun getBindingRoot(): View {
+        binding = ActivityItemListBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun initView(savedInstanceState: Bundle?) {
+
+    }
+
+}
